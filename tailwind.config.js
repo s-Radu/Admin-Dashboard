63ccac8f052eb95e9c74ae7e3ef21f5f3f1d9598
lil.void <tsraduu@gmail.com>
2023-12-03 13:41:29 +0200

    header nearly done
    takes longer to complete it with tailwind as I need to go through calsses and find out which ones i really need and getting used to them

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  darkMode: "class",
+  content: ["./index.html", "./src/app.js"],
   theme: {
+    fontFamily: {
+      josefin: ["Josefin Sans", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+      nunito: ["Nunito Sans", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
