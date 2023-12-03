45dfb46bb9e289e3cc9ffdf9f17d21fef322f270
lil.void <tsraduu@gmail.com>
2023-12-03 12:06:34 +0200

    Initial commit, tw initialised

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+//* ussage command for --watch mode : npx tailwindcss -i ./src/input.css -o ./src/output.css -w
