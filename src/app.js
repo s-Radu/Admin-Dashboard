35c01e57c942b41d9c4e7c84a0970c78922d8b2c
lil.void <tsraduu@gmail.com>
2023-12-06 20:26:07 +0000

    Added functinality to New button
    I was thinking of adding the new projects with appendChild, but that would be a lot of work,
    Asked copilot how should i go about this and it introduced insertAdjacentHTML which is really helpful when creating new elements and knowing their class, id's and everything that's needed in order to behave as requested

--- src/app.js
+++ src/app.js
@@ -6,9 +6,28 @@ const searchInput = document.getElementById("text");
 const projectLike = document.querySelectorAll(".like");
 const shareBtn = document.getElementById("btn-share");
 const sharedMessage = document.getElementById("shared-message");
-//> Woth with fa-solid and fa-regular
+const newProject = document.getElementById("btn-new");
 
 //> functions
+
+function newProjectFunction() {
+  const projectWrapper = document.getElementById("project-wrapper");
+  const title = prompt("What's the project title?");
+  const description = prompt("The project description?");
+
+  const newProjectHTML = `<div class="rounded-2xl border-l-4 border-l-orange-400 bg-slate-600 flex flex-col justify-between">
+    <div>
+      <h2 class="font-montserat ml-2 text-4xl m-4">${title}</h2>
+      <p class="font-montserat ml-2 text-lg indent-2 m-4">${description}</p>
+    </div>
+    <div class="grid-cols-1 grid place-items-end grid-flow-col gap-4 m-4">
+      <i class="fa-regular fa-star text-2xl like cursor-pointer"></i>
+      <i class="fa-regular fa-eye text-2xl cursor-pointer"></i>
+      <i class="fa-solid fa-code-fork text-2xl cursor-pointer"></i>
+    </div>
+  </div>`;
+  projectWrapper.insertAdjacentHTML("beforeend", newProjectHTML);
+}
 
 function shared() {
   navigator.clipboard
@@ -85,3 +104,5 @@ projectLike.forEach((project) => {
 });
 
 shareBtn.addEventListener("click", shared);
+
+newProject.addEventListener("click", newProjectFunction);
