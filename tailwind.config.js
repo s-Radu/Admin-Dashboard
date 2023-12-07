f58bf312ab7601386dc01e57ed909cf9cd1e2505
lil.void <tsraduu@gmail.com>
2023-12-07 18:02:05 +0000

    Added icons to nav bar and grow animation on projects

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html", "./src/app.js"],
+  theme: {
+    fontFamily: {
+      josefin: ["Josefin Sans", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+      nunito: ["Nunito Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+  purge: ["./index.html", "./src/app.js"],
+};
+
+//* ussage command for --watch mode : npx tailwindcss -i ./src/input.css -o ./src/output.css -w
+
+//! ADD PURGE AS YOU FORGOT
