cae6b1aa85be3a9563b0c176d6837681dae80a65
lil.void <tsraduu@gmail.com>
2023-12-08 17:30:29 +0000

    added text shadow to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/app.js"],
   theme: {
+    extend: {},
+    textShadow: {
+      sm: "0 0  4px var(--tw-shadow-color)",
+      md: "0 0  8px var(--tw-shadow-color)",
+      lg: "0 0  12px var(--tw-shadow-color)",
+      xl: "0 0  16px var(--tw-shadow-color)",
+    },
     fontFamily: {
       josefin: ["Josefin Sans", "sans-serif"],
       montserrat: ["Montserrat", "sans-serif"],
       nunito: ["Nunito Sans", "sans-serif"],
     },
-    extend: {},
   },
-  plugins: [],
-  purge: ["./index.html", "./src/app.js"],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
+
+  purge: {
+    cotent: ["./index.html", "./src/app.js"],
+  },
 };
 
 //* ussage command for --watch mode : npx tailwindcss -i ./src/input.css -o ./src/output.css -w
